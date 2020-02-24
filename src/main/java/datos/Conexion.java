87375f2ffd6d70258e1cfd81c645a7a97440f314
David V. Martínez <48252203+davidvemtz@users.noreply.github.com>
2020-02-24 09:54:03 -0600

    Update Conexion.java

--- src/main/java/datos/Conexion.java
+++ src/main/java/datos/Conexion.java
@@ -8,8 +8,8 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class Conexion {
 
    private static final String JDBC_URL = "jdbc:mysql://localhost:3030/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-   private static final String JDBC_USER = "root";
-   private static final String JDBC_PASSWORD = "admin";
+   private static final String JDBC_USER = "???????????????";
+   private static final String JDBC_PASSWORD = "???????????????";
 
    private static BasicDataSource dataSource;
 
